52e776d68b2b80c932854e618552db6a0c994fbe
Justin Rubek <justintrubek@protonmail.com>
2022-06-10 01:29:04 -0500

    Remove lastplace lua file

--- programs/nvim/lua/lastplace.lua
+++
@@ -1,9 +0,0 @@
-local lastplace = require("nvim-lastplace")
-
-lastplace.setup({
-    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-    lastplace_inore_filetype = {
-        "gitcommit", "gitrebase", "svn", "hgcommit",
-    },
-    lastplace_open_folds = true,
-})
