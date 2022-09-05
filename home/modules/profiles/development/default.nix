7a1e996d1b01ebc62caadd0446cc63cc925498d3
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-09-05 03:58:32 -0500

    git configuration

--- home/modules/profiles/development/default.nix
+++ home/modules/profiles/development/default.nix
@@ -15,9 +15,27 @@ in {
       git = {
         enable = true;
         package = pkgs.gitFull;
+
         userName = "Justin Rubek";
         userEmail = "25621857+justinrubek@users.noreply.github.com";
+
         delta.enable = true;
+
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.rebase = false;
+        };
+
+        aliases = {
+          sw = "switch";
+          co = "checkout";
+          ci = "commit";
+          st = "status";
+          br = "branch";
+          ps = "push";
+          pl = "pull";
+          graph = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold cyan)%h%C(reset) - %C(green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+        };
       };
 
       broot = {
