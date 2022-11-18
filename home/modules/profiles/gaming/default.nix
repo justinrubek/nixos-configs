9a2ca45fae5263b04518cb0be9bbdc8365f840df
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-11-18 17:56:23 -0600

    chore: add mangohud

--- home/modules/profiles/gaming/default.nix
+++ home/modules/profiles/gaming/default.nix
@@ -11,11 +11,12 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      protontricks
-      teamspeak_client
+    home.packages = [
+      pkgs.protontricks
+      pkgs.mangohud
+      pkgs.teamspeak_client
       # see https://github.com/NixOS/nixpkgs/issues/78961
-      (discord.override {
+      (pkgs.discord.override {
         nss = pkgs.nss_latest;
         withOpenASAR = true;
       })
