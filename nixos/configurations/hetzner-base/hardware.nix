daa17aa48fd731e97d618ec0f9ba9120b0ca32f5
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-12-12 03:19:11 -0600

    fix: duplicate

--- nixos/configurations/hetzner-base/hardware.nix
+++ nixos/configurations/hetzner-base/hardware.nix
@@ -9,9 +9,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  # Required by zfs to exist
-  networking.hostId = "deadbeef";
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
