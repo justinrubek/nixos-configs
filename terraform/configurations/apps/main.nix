5df06e573eea331da28196f0a854945e1ec17f2d
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-12-26 01:12:27 -0600

    feat: remove dummy api

--- terraform/configurations/apps/main.nix
+++ terraform/configurations/apps/main.nix
@@ -12,16 +12,6 @@ in {
     nomad = {};
   };
 
-  resource.nomad_job.dummy_api = {
-    jobspec = ''''${file("${nomad_jobs}/dummy_api.json")}'';
-    json = true;
-  };
-
-  resource.nomad_job.dummy_api_nix = {
-    jobspec = ''''${file("${nomad_jobs}/dummy_api_nix.json")}'';
-    json = true;
-  };
-
   resource.nomad_job.rubek_site = {
     jobspec = ''''${file("${nomad_jobs}/rubek_site.json")}'';
     json = true;
