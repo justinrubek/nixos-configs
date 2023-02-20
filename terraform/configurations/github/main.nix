ba8e0169e04394899c520852c4bc59edd4ce64b2
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-02-19 21:45:53 -0600

    feat(terraform): added github repository management config

---
+++ terraform/configurations/github/main.nix
@@ -0,0 +1,32 @@
+{
+  nomadJobs,
+  pkgs,
+  ...
+}: let
+  system = pkgs.system;
+
+  nomad_jobs = nomadJobs;
+in {
+  # configure hcloud
+  provider = {
+    github = {};
+  };
+
+  resource.github_repository."lockpad" = {
+    name = "lockpad";
+    description = "Simplistic login system";
+
+    visibility = "public";
+
+    has_issues = true;
+    has_discussions = true;
+
+    # Enable rebase merge only
+    allow_merge_commit = false;
+    allow_squash_merge = false;
+    allow_rebase_merge = true;
+    allow_auto_merge = true;
+
+    delete_branch_on_merge = true;
+  };
+}
