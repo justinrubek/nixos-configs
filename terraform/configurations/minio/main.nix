32def543f9d30db61fab2110d5ce2dff0b8e1a32
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-08-20 13:24:39 -0500

    feat(terraform/minio): add nix-cache user

--- terraform/configurations/minio/main.nix
+++ terraform/configurations/minio/main.nix
@@ -12,10 +12,32 @@
     };
   };
 
+  resource.minio_iam_user = {
+    nix_cache = {
+      name = "nix-cache";
+      force_destroy = true;
+    };
+  };
+
+  resource.minio_iam_service_account = {
+    nix_cache = {
+      target_user = "\${minio_iam_user.nix_cache.name}";
+    };
+  };
+
   # terraform configuration outputs
   output = {
     bucket_nix_cache_name = {
       value = "\${minio_s3_bucket.nix_cache.bucket}";
+    };
+
+    user_nix_cache_access_key = {
+      value = "\${minio_iam_service_account.nix_cache.access_key}";
+    };
+
+    user_nix_cache_secret_key = {
+      value = "\${minio_iam_service_account.nix_cache.secret_key}";
+      sensitive = true;
     };
   };
 }
