35f48ba6b9526e211d0e73a26fc6136da8222ce8
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-08-27 20:48:48 -0500

    chore(terraform/minio): added testing bucket and user

--- terraform/configurations/minio/main.nix
+++ terraform/configurations/minio/main.nix
@@ -10,6 +10,10 @@
     nix_cache = {
       bucket = "nix-cache";
     };
+
+    scratch_files = {
+      bucket = "scratch-files";
+    };
   };
 
   resource.minio_iam_user = {
@@ -17,11 +21,93 @@
       name = "nix-cache";
       force_destroy = true;
     };
+
+    justin = {
+      name = "justin";
+      force_destroy = true;
+    };
   };
 
   resource.minio_iam_service_account = {
     nix_cache = {
       target_user = "\${minio_iam_user.nix_cache.name}";
+    };
+
+    justin = {
+      target_user = "\${minio_iam_user.justin.name}";
+    };
+  };
+
+  resource.minio_iam_group = {
+    nix-cache = {
+      name = "nix-cache";
+    };
+
+    justin = {
+      name = "justin";
+    };
+  };
+
+  resource.minio_iam_group_membership = {
+    nix-cache = {
+      name = "nix-cache";
+      group = "\${minio_iam_group.nix-cache.name}";
+      users = [
+        "\${minio_iam_user.nix_cache.name}"
+      ];
+    };
+
+    justin = {
+      name = "justin";
+      group = "\${minio_iam_group.justin.name}";
+      users = [
+        "\${minio_iam_user.justin.name}"
+      ];
+    };
+  };
+
+  resource.minio_iam_group_policy = {
+    read-write-nix-cache = {
+      name = "read-write-nix-cache";
+      group = "\${minio_iam_group.nix-cache.name}";
+      policy = ''
+        {
+          "Version": "2012-10-17",
+          "Statement": [
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "s3:*"
+                  ],
+                  "Resource": [
+                      "arn:aws:s3:::''${minio_s3_bucket.nix_cache.bucket}/*"
+                  ]
+              }
+          ]
+        }
+      '';
+    };
+
+    # grant all
+    justin = {
+      name = "justin";
+      group = "\${minio_iam_group.justin.name}";
+      policy = ''
+        {
+          "Version": "2012-10-17",
+          "Statement": [
+              {
+                  "Effect": "Allow",
+                  "Action": [
+                      "s3:*"
+                  ],
+                  "Resource": [
+                      "arn:aws:s3:::*"
+                  ]
+              }
+          ]
+        }
+      '';
     };
   };
 
@@ -37,6 +123,15 @@
 
     user_nix_cache_secret_key = {
       value = "\${minio_iam_service_account.nix_cache.secret_key}";
+      sensitive = true;
+    };
+
+    user_justin_access_key = {
+      value = "\${minio_iam_service_account.justin.access_key}";
+    };
+
+    user_justin_secret_key = {
+      value = "\${minio_iam_service_account.justin.secret_key}";
       sensitive = true;
     };
   };
