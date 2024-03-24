4071ff8cd9f5b59e8712f26206136c5a9f120d42
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-03-24 16:50:28 -0500

    refactor(hyprland): replace extraConfig with settings

--- home/modules/windowing/hyprland/default.nix
+++ home/modules/windowing/hyprland/default.nix
@@ -35,9 +35,6 @@ in {
       pkgs.wireplumber
       pkgs.wl-clipboard
     ];
-
-    wayland.windowManager.hyprland.enable = true;
-
     programs.wlogout = {
       enable = true;
       layout = [
@@ -68,176 +65,157 @@ in {
       ];
     };
 
-    wayland.windowManager.hyprland.extraConfig = ''
-      $mod = SUPER
-      $modalt = SUPER_ALT
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        "$mod" = "SUPER";
+        "$modalt" = "SUPER_ALT";
 
-      # TODO: better monitor configuration
-      monitor = HDMI-A-1, 2560x1440@100, 1920x0, auto
-      monitor = DP-1, 1920x1080@144, 0x0, auto
-      workspace = HDMI-A-1, 1
-      workspace = DP-1, 10
+        animations = {
+          enabled = true;
+          animation = [
+            "border, 1, 2, default"
+            "fade, 1, 4, default"
+            "windows, 1, 3, default, popin 80%"
+            "workspaces, 1, 2, default, slide"
+          ];
+        };
+        bind =
+          [
+            "$mod, Return, exec, ${apps.terminal}"
+            "$mod, Space, exec, ${apps.launcher}"
+            "$mod, Escape, exec, wlogout -p layer-shell" # logout
+            "$mod, E, exec, ${apps.emoji}" # emoji picker
+            # compositor
+            "$modalt, M, exit,"
+            "$mod, Q, killactive,"
+            "$mod, F, fullscreen,"
+            "$mod, R, togglesplit,"
+            "$mod, T, togglefloating,"
+            "$mod, P, pseudo"
+            # focus navigation
+            "$mod, h, movefocus, l"
+            "$mod, l, movefocus, r"
+            "$mod, k, movefocus, u"
+            "$mod, j, movefocus, d"
+            # grouped windows
+            "$mod, G, togglegroup,"
+            "$mod SHIFT, L, changegroupactive, f"
+            "$mod SHIFT, H, changegroupactive, b"
+            # special workspace
+            "$mod SHIFT, grave, movetoworkspace, special"
+            "$mod, grave, togglespecialworkspace"
+            # cycle through workspaces
+            "$mod, bracketleft, workspace, m-1"
+            "$mod, bracketright, workspace, m+1"
+            # cycle through monitors
+            "$mod SHIFT, bracketleft, focusmonitor, l"
+            "$mod SHIFT, bracketright, focusmonitor, r"
+          ]
+          ++ (builtins.genList (
+              x: let
+                s = toString x;
+              in ''
+                bind = $mod, ${s}, workspace, ${s}
+              ''
+            )
+            10)
+          ++ (builtins.genList (
+              x: let
+                s = toString x;
+              in ''
+                bind = $mod SHIFT, ${s}, movetoworkspace, ${s}
+              ''
+            )
+            10);
 
-      misc {
-        # enable variable frame rate
-        vfr = 1
+        bindl = [
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioStop, exec, playerctl stop"
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPrev, exec, playerctl previous"
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ];
+        bindle = [
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ];
+        bindm = [
+          "$mod, mouse:272, movewindow"
+          "$mod, mouse:273, resizewindow"
+        ];
+        decoration = {
+          rounding = 16;
+          blur = {
+            enabled = true;
+            size = 3;
+            passes = 3;
+            new_optimizations = true;
+          };
+          drop_shadow = true;
+          shadow_ignore_window = true;
+          shadow_offset = "2 2";
+          shadow_range = 4;
+          shadow_render_power = 1;
+          "col.shadow" = "0x55000000";
+        };
+        dwindle = {
+          pseudotile = true;
+          preserve_split = true;
+        };
+        general = {
+          border_size = 2;
+          gaps_in = 5;
+          gaps_out = 5;
+          layout = "dwindle";
 
-        # no need for auto-reload on nix
-        disable_autoreload = 1
+          "col.active_border" = "rgb(${colors.blue}) rgb(${colors.mauve}) 270deg";
+          "col.inactive_border" = "rgb(${colors.crust}) rgb(${colors.lavender}) 270deg";
+        };
+        group = {
+          groupbar = {
+            font_size = 12;
+            gradients = false;
+          };
 
-        # focus_on_activate = 1
-      }
-
-      input {
-        kb_layout = us
-
-        # focus windows on mouse hover
-        follow_mouse = 1
-        # fix for popups (e.g. steam)
-        mouse_refocus = 0
-
-        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-      }
-
-      general {
-        gaps_in = 5
-        gaps_out = 5
-        border_size = 2
-
-        col.active_border = rgb(${colors.blue}) rgb(${colors.mauve}) 270deg
-        col.inactive_border = rgb(${colors.crust}) rgb(${colors.lavender}) 270deg
-
-        layout = dwindle
-      }
-
-      group {
-        col.border_active = rgb(${colors.pink})
-        col.border_inactive = rgb(${colors.surface0})
-      }
-
-      decoration {
-        rounding = 16
-        blur {
-          enabled = true
-          size = 3
-          passes = 3
-          new_optimizations = true
-        }
-
-        drop_shadow = 1
-        shadow_ignore_window = 1
-        shadow_offset = 2 2
-        shadow_range = 4
-        shadow_render_power = 1
-        col.shadow = 0x55000000
-      }
-
-      animations {
-        enabled = 1
-        animation = border, 1, 2, default
-        animation = fade, 1, 4, default
-        animation = windows, 1, 3, default, popin 80%
-        animation = workspaces, 1, 2, default, slide
-      }
-
-      dwindle {
-        pseudotile = 1
-        preserve_split = 1
-      }
-
-      # disable idle when watching video
-      windowrulev2 = idleinhibit fullscreen, class:^(firefox)$
-
-      # floating windows
-      windowrulev2 = float,title:(SpeedCrunch)
-      windowrulev2 = size 50% 50%,title:(SpeedCrunch)
-
-      # workspaced windows
-      windowrulev2 = workspace 9, class:(.*iscord.*)
-      windowrulev2 = workspace 9, title:(Teamspeak 3)
-
-      windowrulev2 = workspace 5 silent, class:(steam)
-      windowrulev2 = workspace special silent, class:(steam), title:(Sign in to Steam)
-      windowrulev2 = float, class:(steam), title:(Friends List)
-      windowrulev2 = size 400 600, class:(steam), title:(Friends List)
-
-      # launch terminal
-      bind = $mod, Return, exec, ${apps.terminal}
-      bind = $mod, Space, exec, ${apps.launcher}
-
-      # mouse manipulation
-      # https://wiki.hyprland.org/Configuring/Binds/#mouse-binds
-      bindm = $mod, mouse:272, movewindow
-      bindm = $mod, mouse:273, resizewindow
-
-      # windowing commands
-      #  compositor
-      bind = $modalt, M, exit,
-      bind = $mod, Q, killactive,
-      bind = $mod, F, fullscreen,
-      bind = $mod, R, togglesplit,
-      bind = $mod, T, togglefloating,
-      bind = $mod, P, pseudo
-      #  focus navigation
-      bind = $mod, h, movefocus, l
-      bind = $mod, l, movefocus, r
-      bind = $mod, k, movefocus, u
-      bind = $mod, j, movefocus, d
-      #  grouped windows
-      bind = $mod, G, togglegroup,
-      bind = $mod SHIFT, L, changegroupactive, f
-      bind = $mod SHIFT, H, changegroupactive, b
-      #  logout
-      bind = $mod, Escape, exec, wlogout -p layer-shell
-      #  emoji picker
-      bind = $mod, E, exec, ${apps.emoji}
-
-      # media
-      bindl = , XF86AudioPlay, exec, playerctl play-pause
-      bindl = , XF86AudioStop, exec, playerctl stop
-      bindl = , XF86AudioNext, exec, playerctl next
-      bindl = , XF86AudioPrev, exec, playerctl previous
-      bindle = , XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-      bindle = , XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-      bindl = , XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-      bindl = , XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-
-      # workspaces
-      # binds mod + [shift] {1..10} to [move to] workspace {1..10}
-      ${
-        builtins.concatStringsSep "\n" (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-
-              wsStr = toString (x + 1);
-            in ''
-              bind = $mod, ${ws}, workspace, ${wsStr}
-              bind = $mod SHIFT, ${ws}, movetoworkspace, ${wsStr}
-            ''
-          )
-          10)
-      }
-
-      # workspace assignments
-      #  Discord on workspace 2
-      windowrulev2 = workspace 2, title:^(Discord)$
-      #  teamspeak on workspace 3
-      windowrulev2 = workspace 3, class:^(ts3client)$
-
-      # special workspace
-      bind = $mod SHIFT, grave, movetoworkspace, special
-      bind = $mod, grave, togglespecialworkspace
-
-      # cycle through workspaces
-      bind = $mod, bracketleft, workspace, m-1
-      bind = $mod, bracketright, workspace, m+1
-
-      # cycle through monitors
-      bind = $mod SHIFT, bracketleft, focusmonitor, l
-      bind = $mod SHIFT, bracketright, focusmonitor, r
-    '';
+          "col.border_active" = "${colors.pink}";
+          "col.border_inactive" = "${colors.surface0}";
+        };
+        input = {
+          kb_layout = "us";
+          follow_mouse = true;
+          mouse_refocus = false;
+          sensitivity = 0;
+        };
+        misc = {
+          vfr = true;
+          disable_autoreload = true; # no need for auto-reload on nix
+        };
+        monitor = [
+          "HDMI-A-1, 2560x1440@100, 1920x0, auto"
+          "DP-1, 1920x1080@144, 0x0, auto"
+        ];
+        windowrulev2 = [
+          # disable idle when watching video
+          "idleinhibit fullscreen, class:^(firefox)$"
+          # floating windows
+          "float,title:(SpeedCrunch)"
+          "size 50% 50%,title:(SpeedCrunch)"
+          # workspaced windows
+          "workspace 9, class:(.*iscord.*)"
+          "workspace 9, title:(Teamspeak 3)"
+          # steam
+          "workspace 5 silent, class:(steam)"
+          "workspace special silent, class:(steam), title:(Sign in to Steam)"
+          "float, class:(steam), title:(Friends List)"
+          "size 400 600, class:(steam), title:(Friends List)"
+        ];
+        workspace = [
+          "HDMI-A-1, 1"
+          "DP-1, 10"
+        ];
+      };
+    };
   };
 }
