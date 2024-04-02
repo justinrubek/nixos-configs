09e64773a6c5d6fea759cf264912aaec0f15ddcc
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-01 23:13:15 -0500

    chore: tweak gaming packages

--- home/modules/profiles/gaming/default.nix
+++ home/modules/profiles/gaming/default.nix
@@ -13,6 +13,7 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = [
       pkgs.protontricks
+      pkgs.winetricks
       pkgs.mangohud
       pkgs.teamspeak_client
       # see https://github.com/NixOS/nixpkgs/issues/78961
@@ -20,6 +21,7 @@ in {
         nss = pkgs.nss_latest;
         # withOpenASAR = true;
       })
+      pkgs.webcord
       pkgs.airshipper
       # pkgs.runelite
       pkgs.teamspeak5_client
