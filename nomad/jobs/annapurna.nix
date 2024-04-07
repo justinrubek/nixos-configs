fab43ee0fe5100bd9c0a182965a5722529eac791
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-07 18:28:26 -0500

    feat(terraform/apps): deploy annapurna with postgres

--- nomad/jobs/annapurna.nix
+++ nomad/jobs/annapurna.nix
@@ -1,7 +1,11 @@
 {...}: let
-  annapurna-image = "ghcr.io/justinrubek/annapurna:7d2faebf0ff2051cc392893a15e63ac14c2553d7";
+  annapurna-image = "ghcr.io/justinrubek/annapurna:e5e48732bf6c2c2ea4e8144a9a3b2bbce6905ba9";
 
   lockpadSecret = name: ''{{ with secret "kv-v2/data/annapurna/lockpad" }}{{ .Data.data.${name} | toJSON }}{{ end }}'';
+
+  postgresKey = "annapurna/postgres";
+  postgresSecret = name: ''{{ with secret "kv-v2/data/${postgresKey}" }}{{ .Data.data.${name} }}{{ end }}'';
+  postgresUrl = ''postgres://${postgresSecret "username"}:${postgresSecret "password"}@alex:5435/${postgresSecret "database"}'';
 in {
   job.annapurna = {
     datacenters = ["dc1"];
@@ -15,6 +19,41 @@ in {
         }
       ];
 
+      task.database_migration = {
+        lifecycle = {
+          hook = "prestart";
+          sidecar = false;
+        };
+
+        driver = "docker";
+        config = {
+          image = annapurna-image;
+          command = "sqlx";
+          args = [
+            "migrate"
+            "run"
+            "--source"
+            "/migrations"
+            "--connect-timeout"
+            "120"
+          ];
+        };
+
+        vault = {
+          policies = ["annapurna-postgres"];
+        };
+
+        templates = [
+          {
+            data = ''
+              DATABASE_URL=${postgresUrl}
+            '';
+            destination = "local/env";
+            env = true;
+          }
+        ];
+      };
+
       task.backend = {
         driver = "docker";
 
@@ -22,6 +61,7 @@ in {
           image = annapurna-image;
 
           ports = ["http"];
+          command = "annapurna-cli";
           args = [
             "server"
             "http"
@@ -29,7 +69,7 @@ in {
         };
 
         vault = {
-          policies = ["annapurna"];
+          policies = ["annapurna" "annapurna-postgres"];
         };
 
         templates = [
@@ -38,6 +78,7 @@ in {
               ANNAPURNA_AUTH_APP_ID=${lockpadSecret "application_id"}
               ANNAPURNA_AUTH_URL=${lockpadSecret "url"}
               ANNAPURNA_FACTS_PATH=/facts
+              ANNAPURNA_POSTGRES_URL=${postgresUrl}
               ANNAPURNA_STATIC_PATH=/public
             '';
             destination = "secrets/env";
