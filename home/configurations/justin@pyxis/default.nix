23625aaee58e081c17d21843816727f6dbcefded
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-15 09:32:34 -0500

    chore: cleanup server homeConfigurations
    They do not need comma nor zellij to be specified directly.

--- home/configurations/justin@pyxis/default.nix
+++ home/configurations/justin@pyxis/default.nix
@@ -1,17 +1,5 @@
-{comma, ...}: {pkgs, ...}: {
+_inputs: {pkgs, ...}: {
   config = {
     activeProfiles = ["development"];
-
-    home.packages = with pkgs; [
-      comma.packages.x86_64-linux.default
-      alejandra
-    ];
-
-    programs.zellij = {
-      enable = true;
-      settings = {
-        default-shell = "zsh";
-      };
-    };
   };
 }
