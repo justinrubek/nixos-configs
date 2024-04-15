6f3b03c7fd0bf23ca43fd3dc769a8b0c4b637633
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-15 18:30:56 -0500

    feat(installer-image): add separate graphical and minimal packages

--- packages/installer-image.nix
+++ packages/installer-image.nix
@@ -1,25 +1,26 @@
-{
-  config,
-  inputs,
-  lib,
-  self,
-  ...
-}: {
+{inputs, ...}: {
   perSystem = {
     config,
     pkgs,
     system,
     ...
   }: let
-    iso = inputs.nixpkgs.lib.nixosSystem {
+    graphical = inputs.nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
         "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma5.nix"
       ];
     };
+    minimal = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+      ];
+    };
   in rec {
     packages = {
-      iso = iso.config.system.build.isoImage;
+      "installer/graphical" = graphical.config.system.build.isoImage;
+      "installer/minimal" = minimal.config.system.build.isoImage;
     };
   };
 }
