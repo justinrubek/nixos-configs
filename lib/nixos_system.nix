d790054e665e4b8c34ec45407d63ea98848ea816
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-29 22:56:31 -0500

    fix: use en_GB.UTF8 for nixos LC_TIME
    This ensures that a 24-hour clock is used instead of a 12 hour one.

--- lib/nixos_system.nix
+++ lib/nixos_system.nix
@@ -30,6 +30,10 @@ input @ {
 
         caches.enable = true;
         nix.flakes.enable = true;
+
+        i18n.extraLocaleSettings = {
+          LC_TIME = "en_GB.UTF-8";
+        };
       }
     ]
     ++ modules
