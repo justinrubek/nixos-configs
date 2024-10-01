e64eec6f3150448e439df545d8032f0b59a458b1
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-10-01 13:53:48 -0500

    fixup! feat: darwin configuration

--- flake-parts/shells.nix
+++ flake-parts/shells.nix
@@ -17,11 +17,14 @@
   in {
     devShells = {
       default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          inputs.home-manager.packages.${system}.home-manager
-          pkgs.nh
-          inputs'.nix-darwin.packages.darwin-rebuild
-        ];
+        buildInputs =
+          [
+            inputs.home-manager.packages.${system}.home-manager
+            pkgs.nh
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            inputs'.nix-darwin.packages.darwin-rebuild
+          ];
 
         shellHook = ''
           ${config.pre-commit.installationScript}
