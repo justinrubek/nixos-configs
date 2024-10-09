bb645023520135daa2cf579a7d371f1c7a0b54f6
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-10-09 09:14:00 -0500

    feat: aws access

--- home/configurations/justin@M-JWW1JWQ5PW/default.nix
+++ home/configurations/justin@M-JWW1JWQ5PW/default.nix
@@ -10,7 +10,7 @@ inputs: {
       packages = with pkgs; [
         pkgs.tokei
         inputs.nix-go.packages.${pkgs.system}.go
-        pkgs.awscli
+        pkgs.awscli2
         pkgs.kind
         inputs.nixpkgs-2405.legacyPackages.${pkgs.system}.kops_1_26
         pkgs.kubectl
@@ -19,11 +19,37 @@ inputs: {
         pkgs.sops
         pkgs.terraform
         pkgs.vfkit
+        pkgs.yq
       ];
 
       sessionPath = ["$HOME/go/bin"];
 
       stateVersion = "24.05";
     };
+
+    programs.zsh.initExtra = ''
+      awsprofile() {
+        unset AWS_ACCESS_KEY AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+        if (( $# != 2 )); then
+          echo 'Usage:' >&2
+          echo '  awsprofile <source profile> <target profile>' >&2
+          echo >&2
+          echo 'e.g.:' >&2
+          echo '  awsprofile sso admin' >&2
+          echo '  awsprofile sso-ro prod-ro' >&2
+          return
+        fi
+        eval $(aws sts assume-role --profile $1                 \
+          --role-session-name $(whoami)                         \
+          --role-arn $(aws configure get role_arn --profile $2) |
+            jq -r '.Credentials                                 |"
+              export AWS_ACCESS_KEY=\(.AccessKeyId)
+              export AWS_ACCESS_KEY_ID=\(.AccessKeyId)
+              export AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)
+              export AWS_SESSION_TOKEN=\(.SessionToken)
+          "')
+        export AWS_ACCOUNT=$2
+      }
+    '';
   };
 }
