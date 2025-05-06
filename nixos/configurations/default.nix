d2bf2929787a25a3805b9b3b341c038b8b8ff145
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-05-05 21:06:16 -0500

    chore: default locale

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -17,8 +17,11 @@
             enable = true;
             generateCaches = true;
           };
-          i18n.extraLocaleSettings = {
-            LC_TIME = "en_GB.UTF-8";
+          i18n = {
+            defaultLocale = "en_US.UTF-8";
+            extraLocaleSettings = {
+              LC_TIME = "en_GB.UTF-8";
+            };
           };
           nix.settings.trusted-users = ["@wheel"];
         }
