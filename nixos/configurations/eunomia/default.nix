1ff49a0996bfdda054714a39ccfa99a5b9152b09
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-06-08 22:25:12 -0500

    chore: enable fish by default

--- nixos/configurations/eunomia/default.nix
+++ nixos/configurations/eunomia/default.nix
@@ -159,7 +159,7 @@
           "dialout"
           "stowage"
         ];
-        shell = pkgs.zsh;
+        shell = pkgs.fish;
       };
     };
   };
@@ -232,7 +232,86 @@
   };
 
   programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set -U fish_greeting
+        set -gx EDITOR nvim
+        set -gx VISUAL nvim
+        fish_vi_key_bindings
+
+        set -l foreground c0caf5
+        set -l selection 283457
+        set -l comment 565f89
+        set -l red f7768e
+        set -l orange ff9e64
+        set -l yellow e0af68
+        set -l green 9ece6a
+        set -l purple 9d7cd8
+        set -l cyan 7dcfff
+        set -l pink bb9af7
+
+        set -g fish_color_normal $foreground
+        set -g fish_color_command $cyan
+        set -g fish_color_keyword $pink
+        set -g fish_color_quote $yellow
+        set -g fish_color_redirection $foreground
+        set -g fish_color_end $orange
+        set -g fish_color_option $pink
+        set -g fish_color_error $red
+        set -g fish_color_param $purple
+        set -g fish_color_comment $comment
+        set -g fish_color_selection --background=$selection
+        set -g fish_color_search_match --background=$selection
+        set -g fish_color_operator $green
+        set -g fish_color_escape $pink
+        set -g fish_color_autosuggestion $comment
+
+        set -g fish_pager_color_progress $comment
+        set -g fish_pager_color_prefix $cyan
+        set -g fish_pager_color_completion $foreground
+        set -g fish_pager_color_description $comment
+        set -g fish_pager_color_selected_background --background=$selection
+
+        starship init fish | source
+      '';
+
+      shellAbbrs.vi = "nvim";
+    };
     gamescope.enable = true;
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[󰇸](bold red)";
+          vicmd_symbol = "[❯](bold green)";
+        };
+        directory = {
+          truncate_to_repo = false;
+        };
+        aws = {
+          disabled = true;
+          symbol = "  ";
+        };
+        buf.symbol = " ";
+        c.symbol = " ";
+        directory.read_only = " ";
+        docker_context.symbol = " ";
+        git_branch.symbol = " ";
+        haskell.symbol = " ";
+        java.symbol = " ";
+        julia.symbol = " ";
+        memory_usage.symbol = " ";
+        nix_shell.symbol = " ";
+        nodejs.symbol = " ";
+        package.symbol = " ";
+        python.symbol = " ";
+        rust.symbol = " ";
+        terraform.symbol = " ";
+      };
+    };
     steam = {
       enable = true;
       remotePlay.openFirewall = true;
@@ -293,6 +372,6 @@
     group = "stowage";
     extraGroups = [];
     packages = [];
-    shell = pkgs.zsh;
+    shell = pkgs.fish;
   };
 }
