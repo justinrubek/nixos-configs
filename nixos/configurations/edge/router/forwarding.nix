fde9fce7284116173cff062dae39db5bc228f950
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-07-30 12:03:42 -0500

    feat: ipv6

--- nixos/configurations/edge/router/forwarding.nix
+++ nixos/configurations/edge/router/forwarding.nix
@@ -5,10 +5,10 @@
     "net.ipv4.conf.enp1s0f0.rp_filter" = 1;
     "net.ipv4.conf.br-lan.rp_filter" = 1;
     "net.ipv4.conf.default.rp_filter" = 1;
-    # no ipv6 yet
+    # ipv6: do not automatically configure addresses by default
     "net.ipv6.conf.all.accept_ra" = 0;
     "net.ipv6.conf.all.autoconf" = 0;
-    "net.ipv6.conf.all.forwarding" = 0;
+    "net.ipv6.conf.all.forwarding" = 1;
     "net.ipv6.conf.all.use_tempaddr" = 0;
   };
 }
