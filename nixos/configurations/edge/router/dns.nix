2ca09df4d581358666660444737ce6e559316088
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-07-30 12:23:34 -0500

    feat: slaac

--- nixos/configurations/edge/router/dns.nix
+++ nixos/configurations/edge/router/dns.nix
@@ -1,4 +1,6 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  ulaPrefix = "fd00:9745:a08b:01e6";
+in {
   services = {
     resolved.enable = false;
     unbound = {
@@ -11,9 +13,13 @@
             name = ".";
             forward-addr = [
               "1.1.1.1@853#cloudflare-dns.com"
+              "2606:4700:4700::1111@853#cloudflare-dns.com"
               "1.0.0.1@853#cloudflare-dns.com"
+              "2606:4700:4700::1001@853#cloudflare-dns.com"
               "8.8.8.8@853#dns.google"
+              "2001:4860:4860::8888@853#dns.google"
               "8.8.4.4@853#dns.google"
+              "2001:4860:4860::8844@853#dns.google"
             ];
             forward-first = false;
             forward-tls-upstream = true;
@@ -30,13 +36,16 @@
             "192.168.0.0/16 allow"
             "172.16.0.0/12 allow"
             "10.0.0.0/8 allow"
+            "${ulaPrefix}::/64 allow"
             "::0/0 refuse"
             "::1 allow"
           ];
           extended-statistics = true;
           interface = [
             "10.0.0.1"
+            "${ulaPrefix}::1"
             "127.0.0.1"
+            "::1"
           ];
           root-hints = "${pkgs.dns-root-data}/root.hints";
         };
--- nixos/configurations/edge/router/lan.nix
+++ nixos/configurations/edge/router/lan.nix
@@ -1,4 +1,6 @@
-{
+let
+  ulaPrefix = "fd00:9745:a08b:01e6";
+in {
   systemd.network = {
     netdevs."20-br-lan".netdevConfig = {
       Kind = "bridge";
@@ -22,11 +24,25 @@
       "10-br-lan" = {
         address = [
           "10.0.0.1/8"
+          "${ulaPrefix}::1/64"
         ];
         bridgeConfig = {};
+        ipv6Prefixes = [
+          {
+            AddressAutoconfiguration = true;
+            Assign = true;
+            OnLink = true;
+            Prefix = "${ulaPrefix}::/64";
+          }
+        ];
         linkConfig.RequiredForOnline = "no";
         matchConfig.Name = "br-lan";
-        networkConfig.ConfigureWithoutCarrier = true;
+        networkConfig = {
+          ConfigureWithoutCarrier = true;
+          DHCPPrefixDelegation = true;
+          IPv6SendRA = true;
+          IPv6AcceptRA = false;
+        };
       };
     };
   };
