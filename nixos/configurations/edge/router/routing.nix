b18ee57afed2d675c9329df8601b50c98085b76f
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-07-29 23:36:38 -0500

    fixup! feat: basic router functionality

--- nixos/configurations/edge/router/routing.nix
+++ nixos/configurations/edge/router/routing.nix
@@ -24,7 +24,7 @@
               type filter hook forward priority filter; policy drop;
 
               iifname "br-lan" oifname "enp1s0f0" accept comment "allow lan to wan as trusted"
-              iifname "enp1s0f0" oifname "br-lan" ct state { established, related accept comment "allow established traffic"
+              iifname "enp1s0f0" oifname "br-lan" ct state { established, related } accept comment "allow established traffic"
               iifname "enp1s0f0" oifname "br-lan" ct status dnat accept comment "allow nat from wan"
             }
           '';
@@ -35,12 +35,12 @@
             chain prerouting {
               type nat hook prerouting priority -100;
 
-              iifname "bond-wan" ip protocol tcp tcp dport 443 dnat to 172.16.0.2:443 "nat traffic from wan to webserver"
+              iifname "enp1s0f0" ip protocol tcp tcp dport 443 dnat to 172.16.0.2:443 comment "nat traffic from wan to webserver"
             }
             chain postrouting {
               type nat hook postrouting priority 100; policy accept;
 
-              ip saddr 172.16.0.0/24 oifname "bond-wan" masquerade comment "masquerade private ip addresses"
+              ip saddr 172.16.0.0/24 oifname "enp1s0f0" masquerade comment "masquerade private ip addresses"
             }
           '';
           family = "ip";
